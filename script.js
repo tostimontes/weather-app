05fc4c2e0ae7c13d92dddd78dfecd91e04833445
tostimontes <147332922+tostimontes@users.noreply.github.com>
2024-01-28 21:20:23 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Write the functions that hit the API. You’re going to want functions that can take a location and return the weather data for that location. For now, just console.log() the information.
+// Write the functions that process the JSON data you’re getting from the API and return an object with only the data you require for your app.
+// Set up a form that will let users input their location and will fetch the weather info (still just console.log() it).
+// Display the information on your webpage!
+// Add any styling you like!
+// Optional: add a ‘loading’ component that displays from the time the form is submitted until the information comes back from the API. Use DevTools to test for low-end devices.
+// WEATHER_API_KEY = ad93355e721349a09c8150136242801
+// url = http://api.weatherapi.com/v1
+// Current API: http://api.weatherapi.com/v1/current.json?key={API_KEY_HERE}&q={LOCATION_HERE}
+// Forecast API: http://api.weatherapi.com/v1/forecast.json?key={API_KEY_HERE}&q={LOCATION_HERE}&days={NUMBER_OF_FORECAST_DAYS (always = 3)}
+//  Latitude and Longitude (Decimal degree) e.g: q=48.8567,2.3508
+//  city name e.g.: q=Paris
+//  auto:ip IP lookup e.g: q=auto:ip
