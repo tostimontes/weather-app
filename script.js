c9bb9e67a37c6de65f99faa1d19fe120b5d995c1
tostimontes <147332922+tostimontes@users.noreply.github.com>
2024-01-31 15:44:16 +0100

    feat: weather fetch and UI update complete
    - Added and configured webpack
    - Added date-fns

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { addDays, format, getDate } from 'date-fns';
+import './style.css';
 // Set up a form that will let users input their location and will fetch the weather info (still just console.log() it).
 // Display the information on your webpage!
 // Add any styling you like!
@@ -105,13 +107,68 @@ async function fetchWeather(selectedLocation) {
 }
 
 const updateUI = (weather) => {
+  const now = new Date();
+  const tomorrow = addDays(now, 1);
+  const dayAfterTomorrow = addDays(now, 2);
   const location = document.querySelector('#location');
+
+  const title1 = document.querySelector('#first_day_title');
+  const icon1 = document.querySelector('#first_day_icon');
+  const condition1 = document.querySelector('#first_day_condition');
+  const temp1 = document.querySelector('#temp_first');
+  const wind1 = document.querySelector('#wind_first');
+  const humidity1 = document.querySelector('#humidity_first');
+  const rain1 = document.querySelector('#rain_first');
+  const sunriseP = document.querySelector('#sunrise');
+  const sunsetP = document.querySelector('#sunset');
+
+  const title2 = document.querySelector('#second_day_title');
+  const icon2 = document.querySelector('#second_day_icon');
+  const condition2 = document.querySelector('#second_day_condition');
+  const temp2 = document.querySelector('#temp_second');
+  const wind2 = document.querySelector('#wind_second');
+  const humidity2 = document.querySelector('#humidity_second');
+  const rain2 = document.querySelector('#rain_second');
+
+  const title3 = document.querySelector('#third_day_title');
+  const icon3 = document.querySelector('#third_day_icon');
+  const condition3 = document.querySelector('#third_day_condition');
+  const temp3 = document.querySelector('#temp_third');
+  const wind3 = document.querySelector('#wind_third');
+  const humidity3 = document.querySelector('#humidity_third');
+  const rain3 = document.querySelector('#rain_third');
 
   location.textContent = `${weather.location.locationName}, ${
     weather.location.locationRegion
   }, ${
     weather.location.locationCountry
   } at ${weather.location.locationTime.slice(-5)} local time`;
+
+  title1.textContent = `Today ${format(now, 'iii d MMM')}`;
+  icon1.src = weather.firstDay.firstDayIcon;
+  condition1.textContent = weather.firstDay.firstDayCondition;
+  temp1.textContent = `${weather.firstDay.firstDayTempC}ºC`;
+  wind1.textContent = `${weather.firstDay.firstDayWind} Km/h`;
+  humidity1.textContent = `${weather.firstDay.firstDayHumidity} %`;
+  rain1.textContent = `${weather.firstDay.firstDayRain} %`;
+  sunriseP.textContent = `${weather.firstDay.sunrise}`;
+
+  title2.textContent = `${format(tomorrow, 'iii d MMM')}`;
+  icon2.src = weather.secondDay.secondDayIcon;
+  condition2.textContent = weather.secondDay.secondDayCondition;
+  temp2.textContent = `${weather.secondDay.secondDayTempC}ºC`;
+  wind2.textContent = `${weather.secondDay.secondDayWind} Km/h`;
+  humidity2.textContent = `${weather.secondDay.secondDayHumidity} %`;
+  rain2.textContent = `${weather.secondDay.secondDayRain} %`;
+
+  title3.textContent = `${format(dayAfterTomorrow, 'iii d MMM')}`;
+  icon3.src = weather.thirdDay.thirdDayIcon;
+  condition3.textContent = weather.thirdDay.thirdDayCondition;
+  temp3.textContent = `${weather.thirdDay.thirdDayTempC}ºC`;
+  wind3.textContent = `${weather.thirdDay.thirdDayWind} Km/h`;
+  humidity3.textContent = `${weather.thirdDay.thirdDayHumidity} %`;
+  rain3.textContent = `${weather.thirdDay.thirdDayRain} %`;
+  sunsetP.textContent = `${weather.firstDay.sunset}`;
 };
 
 // TODO: separate fetch from UI display so that it's not a constant refresh/fetch, only get new data after a time, or after a refresh
@@ -125,7 +182,17 @@ document.querySelector('#search_button').addEventListener('click', () => {
   });
 });
 
-fetchWeather('azpeitia').then((weather) => {
+document.querySelector('#search_location').addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    const searchValue = e.target.value;
+    fetchWeather(searchValue).then((weather) => {
+      updateUI(weather);
+    });
+  }
+});
+
+fetchWeather('auto:ip').then((weather) => {
   updateUI(weather);
 });
 // function fetchWeather() {
---
+++ webpack.common.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './script.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather app',
+      template: './index.html',
+      filename: 'index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+    ],
+  },
+};
